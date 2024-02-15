b34705a16bd8a3b2565df2f058dd9c9d0474e6c7
mukramalpha <mukram.alphatech@gmail.com>
2024-02-15 11:16:39 +0530

    first commit

---
+++ src/main/java/com/alpha/school/AlphaSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/school/AlphaSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
